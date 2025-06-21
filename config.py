ddf73380dfab03b4a25c9437341a054e15ff5744
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-21 16:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(object):
     BOT_TOKEN = "7743917201:AAGBjqqFVK7orEWj2bBO2g13-Ro0Qs_agQ4"
-    DB_NAME = "yassu"
+    DB_NAME = "STAR10"
     API_ID = "27765349"
     API_HASH = "9df1f705c8047ac0d723b29069a1332b"
     ADMIN_ID = "1116405290"
-    DB_URL = "mongodb+srv://yassu:Yash2835P@classxbot.bbodynd.mongodb.net/"
+    DB_URL = "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/"
     LOG_CHANNEL = "https://t.me/+N5xSRah10TZlNmY1"
     USERLINK = ""
     TUTORIAL_VIDEO = ""
